6effc11f055061daa7e21e9b1b1d7ebcd39ee482
Christopher <christopher.d.ferris@gmail.com>
2018-03-28 16:24:33 -0700

    wip

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require('express');
 const path = require('path');
 const serveStatic = require('serve-static');
 
+const expressNunjucks = require('express-nunjucks');
+
 app = express();
-console.log('Serving from: ' + __dirname + '/dist/');
 app.use(serveStatic(__dirname + '/public/'));
 
-app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+const njk = expressNunjucks(app, {
+  watch: true,
+  noCache: true
+});
 
 const port = process.env.PORT || 5000;
 
@@ -17,4 +23,4 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port);
-console.log('server started '+ port);
+console.log(`server started at port ${port}`);
